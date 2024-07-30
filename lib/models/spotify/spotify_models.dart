38947cfa4cb74b8e2f08c73e676c0950f8e22a80
Matthieu Barbe <barbe.matthieu@free.fr>
2024-07-30 02:00:16 +0200

    fix spotify doc

--- lib/models/spotify/spotify_models.dart
+++ lib/models/spotify/spotify_models.dart
@@ -1,4 +1,4 @@
-/// This module contains all the models for app
+/// This module contains all the models needed to communicate with the Spotify API
 library spotify_models;
 
 import '../models_module.dart';
